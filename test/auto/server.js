9b7fda9733a80163c227bf2242deea2cf04869c8
Jared Hanson <jaredhanson@gmail.com>
2013-06-10 16:22:25 -0700

    Reorganize auto tests.

--- test/auto/server.js
+++ test/auto/server.js
@@ -5,4 +5,6 @@ server.use(express.logger())
       .use(express.static(__dirname + '/../www'))
       .use(express.static(__dirname + '/..'))
       .use(express.static(__dirname + '/../..'))
-      .listen(7070);
+      .listen(7070, function() {
+        console.log('HTTP server listening on %s:%d', this.address().address, this.address().port);
+      });
